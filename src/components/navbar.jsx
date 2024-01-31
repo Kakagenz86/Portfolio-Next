80edafa7aa2fb0ec37e24a948f54add8cbf5c4a8
kakagenz <kakagenz86@gmail.com>
2024-01-31 21:35:26 +0800

    add padding navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,9 +5,9 @@ const Navbar = () => {
     return (
       <nav className="bg-slate-900 navbar lg:flex lg:flex-row xs:flex xs:flex-col sticky top-0 gap-3 z-10 justify-around">
               <div className="flex items-center">
-                <h1 className="text-cyan-400 text-xl">Kaka Hardiansyah</h1>
+                <h1 className="text-cyan-400 text-xl ps-3">Kaka Hardiansyah</h1>
               </div>
-              <div className="flex gap-2">
+              <div className="flex gap-2 ps-3">
                   <Link href="#about" className="bg-gray-200 text-black rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</Link>
                   <Link href="#skills" className="text-gray-300 hover:bg-gray-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Skills</Link>
                   <Link href="#project" className="text-gray-300 hover:bg-gray-600 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Project</Link>
